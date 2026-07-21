013572d1a7d66088a6039248e0a378a94002b205
lauzaaa <125464144+nafallauza@users.noreply.github.com>
2026-07-21 20:04:31 +0700

    Update allowed_origins in CORS configurationcro

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', '*')],
+    'allowed_origins' => [env('FRONTEND_URL', 'nikky-frozen-pos-system-nv8kvvupw.vercel.app')],
 
     'allowed_origins_patterns' => [],
 
